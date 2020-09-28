4a029406df388f840d79892a97308dc9be5673f6
DmitriyKondratev1 <conddd@yandex.ru>
2020-09-28 10:45:41 +0300

    2task12week is complete

---
+++ client/components/dashboard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+import Head from './head'
+
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Head title="Hello" />
+      <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+        <div id='title'>
+          Dashboard 
+          <div><Link to='/dashboard/profile/d40f9ccc-df5f-4e63-9880-1b4dff6b2b06'>Go To Profile </Link></div>
+          <div><Link to='/dashboard/main'>Go To Main </Link></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Dashboard.propTypes = {}
+
+export default Dashboard
