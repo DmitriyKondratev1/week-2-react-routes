32e36ea119f4eb03579ee30d43838fed8120ee41
DmitriyKondratev1 <conddd@yandex.ru>
2020-09-28 11:30:23 +0300

    2task12week is complete2

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -9,11 +9,9 @@ const Dashboard = () => {
     <div>
       <Head title="Hello" />
       <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-        <div id='title'>
-          Dashboard 
-          <div><Link to='/dashboard/profile/d40f9ccc-df5f-4e63-9880-1b4dff6b2b06'>Go To Profile </Link></div>
-          <div><Link to='/dashboard/main'>Go To Main </Link></div>
-        </div>
+        <div id="title">Dashboard</div> 
+        <div><Link to='/dashboard/profile/d40f9ccc-df5f-4e63-9880-1b4dff6b2b06'>Go To Profile </Link></div>
+        <div><Link to='/dashboard/main'>Go To Main </Link></div>
       </div>
     </div>
   )
--- client/components/main.js
+++ client/components/main.js
@@ -8,11 +8,9 @@ const Main = () => {
     <div>
       <Head title="Hello" />
       <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-        <div id='title'>
-          Main 
-          <div><Link to='/dashboard/profile/d40f9ccc-df5f-4e63-9880-1b4dff6b2b06'>Go To Profile </Link></div>
-          <div><Link to='/dashboard'>Go To Root </Link></div>
-        </div>
+        <div id="title">Main</div>
+        <div><Link to='/dashboard/profile/d40f9ccc-df5f-4e63-9880-1b4dff6b2b06'>Go To Profile </Link></div>
+        <div><Link to='/dashboard'>Go To Root </Link></div>
       </div>
     </div>
   )
